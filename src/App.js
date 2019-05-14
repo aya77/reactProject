b989cfa98aaca72f3fb4502281828ebacca0871b
rahmaZF <rahmaelzefzafy@gmail.com>
2019-05-14 05:17:58 +0200

    create LeftSide Componant

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import LeftSide from './componants/leftSide' ;
+import { Container, Row, div } from 'reactstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  <Container>
+    <div>
+      <div  className="div-4"> <LeftSide/></div>
+      <div  className="div-4">Sara</div>
+      <div  className="div">Aya</div>
+   </div>
+   </Container> 
   );
 }
 
