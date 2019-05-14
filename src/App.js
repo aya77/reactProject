682ac04c4954d2f1795d1479f457e373e5d2a8de
sara <saraalmasry92@gmail.com>
2019-05-14 15:09:40 +0200

    adding navBar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-// fontAwsome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons'
-library.add(faStroopwafel)
-// end of font awsome
 
+
+import Symbole from './components/symbolsComponent'
 function App() {
   return (
     <div className="App">
-      <div className="raw">
-      <div className="col-4">
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        </div>
-      </div>
+      <Symbole/>
     </div>
   );
 }
--- src/components/symbolsComponent.js
+++ src/components/symbolsComponent.js
@@ -1,19 +1,50 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+
+
+import { Navbar, NavbarBrand, Nav,NavLink } from 'reactstrap';
+// fontAwsome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStroopwafel, faSearch,faUser,  faShoppingBag, faMapMarker  } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faStroopwafel)
+// end of font awsome
 
 class Symbole extends Component {
 
-    
-    render(){
+
+    render() {
         return (
             <div className="container">
-              
+                <div className="raw">
+                    <div className="col-4">
+                        <Navbar bg="secondary" variant="light">
+                            <NavbarBrand href="#home">Navbar</NavbarBrand>
+                            <Nav className="mr-auto">
+                                <NavLink href="#home">
+                                    <FontAwesomeIcon icon={faSearch} />
+                                </NavLink>
+
+                                <NavLink href="#pricing">
+                                    <FontAwesomeIcon icon={faMapMarker} />
+                                </NavLink>
+
+                                <NavLink href="#features">
+                                    <FontAwesomeIcon icon={faShoppingBag} />
+                                </NavLink>
+
+                                <NavLink href="#features">
+                                    <FontAwesomeIcon icon={faUser} />
+                                </NavLink>
+                            </Nav>
+                        </Navbar>
+                    </div>
+                </div>
             </div>
-          );
+        );
 
     }
 
 }
 
-export default Symbol;
+export default Symbole;
