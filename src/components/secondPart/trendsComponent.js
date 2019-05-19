ab74257da7f1e0a3effc9c36a7c17471d3d42716
aya77 <ayalaban888@yahoo.com>
2019-05-19 13:15:01 +0200

    second part

---
+++ src/components/secondPart/trendsComponent.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { TRENDS } from "../../shared/products";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+  Nav
+} from "reactstrap";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import Pagination from "../Pagination";
+export default class TrendsComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: TRENDS,
+      pageOfItems: []
+    };
+    this.onChangePage = this.onChangePage.bind(this);
+  }
+  onChangePage(pageOfItems) {
+    // update state with new page of items
+    this.setState({ pageOfItems: pageOfItems });
+  }
+
+  render() {
+    const products = this.state.pageOfItems.map(item => (
+      <div key={item.id} className="col-12 col-md-4 mt-4">
+        <Card
+          body
+          className="text-center border-0 "
+        >
+          <CardImg
+          height={90}
+            src={item.image}
+            className="card-img-top "
+            alt={item.type}
+          />
+          <CardBody>
+            <CardTitle>{item.title} </CardTitle>
+            <CardText className="text-danger">{item.price} </CardText>
+          </CardBody>
+        </Card>
+      </div>
+    ));
+
+    return (
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-9 d-flex justify-content-left">
+            CURRENTLY TRENDS
+          </div>
+          <div className="col-3 mt-auto">
+            <Pagination
+              items={this.state.products}
+              onChangePage={this.onChangePage}
+              leftIcon={faArrowLeft}
+              centerButton={0}
+              rightIcon={faArrowRight}
+              itemsPerPage={3}
+            />
+          </div>
+        </div>
+        <div className="row">{products}</div>
+      </div>
+    );
+  }
+}
---
+++ src/components/secondPart/typesComponent.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { TYPES } from "../../shared/products";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+  Navbar,
+  NavbarBrand,
+  NavItem,
+  NavLink,
+  Nav
+} from "reactstrap";
+import {
+  faArrowLeft,
+  faArrowRight
+} from "@fortawesome/free-solid-svg-icons";
+import Pagination from "../Pagination";
+export default class TypesComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      TYPES: TYPES,
+      pageOfItems: []
+    };
+    this.onChangePage = this.onChangePage.bind(this);
+  }
+  onChangePage(pageOfItems) {
+    // update state with new page of items
+    this.setState({ pageOfItems: pageOfItems });
+  }
+
+  render() {
+    const types = this.state.pageOfItems.map(item => (
+      <div key={item.id} className="col-12 col-md-4 mt-4">
+        <Card body className="text-left border-0 embed-responsive embed-responsive-16by9">
+          <CardImg src={item.image}  className="card-img-top embed-responsive-item"  alt={item.type} />
+          <CardBody>
+          <CardImgOverlay className="d-flex flex-column justify-content-end">
+          <CardTitle>{item.type}</CardTitle>
+          </CardImgOverlay>
+           
+          </CardBody>
+        </Card>
+      </div>
+    ));
+
+    return (
+      <div className="container mt-3">
+        <div className="row">
+          <div className="col-9 d-flex justify-content-left">
+            WHAT ARE YOU LOOKING FOR?
+          </div>
+          <div className="col-3 mt-auto">
+            <Pagination
+              items={this.state.TYPES}
+              onChangePage={this.onChangePage}
+              leftIcon={faArrowLeft}
+              centerButton={0}
+              rightIcon={faArrowRight}
+              itemsPerPage={3}
+            />
+          </div>
+        </div>
+        <div className="row">{types}</div>
+      </div>
+    );
+  }
+}
