3667c2eb3f9089f4f7d6744a96ba3f887bdc6b66
aya77 <ayalaban888@yahoo.com>
2019-05-30 12:35:54 +0200

    refactor contact icon & product w trend title

--- src/components/seventhPart/contactsComponent.js
+++ src/components/seventhPart/contactsComponent.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMapSigns, faPhone, faClock } from "@fortawesome/free-solid-svg-icons";
+import {
+  faMapSigns,
+  faPhone,
+  faClock
+} from "@fortawesome/free-solid-svg-icons";
 import "../../App.css";
 
 class ContactsComponent extends Component {
   render() {
-
-
     const address = (
       <div className="col-4 col-md-3 p-1 text">
         <div className="row contactsIconBG m-1">
@@ -16,10 +18,10 @@ class ContactsComponent extends Component {
           <strong> Address: </strong>
         </div>
         <div className="row mt-2 text-left text-muted px-3">
-          <>address addressaddress address address address address </>
+          <>Industry Zone , 18 First Section, 12566 October 6, Egypt </>
         </div>
-      </div>)
-
+      </div>
+    );
 
     const contact = (
       <div className="col-4 col-md-3 p-1 ">
@@ -30,11 +32,10 @@ class ContactsComponent extends Component {
           <strong> Contact us: </strong>
         </div>
         <div className="row mt-2 text-left text-muted px-3">
-          <>address addressaddress address address address address </>
+          01201810111-0100054445t m.omran@theoutlet-eg.com
         </div>
       </div>
-
-    )
+    );
 
     const hours = (
       <div className="col-4 col-md-3 p-1">
@@ -45,18 +46,15 @@ class ContactsComponent extends Component {
           <strong> Working Days/Hours: </strong>
         </div>
         <div className="row mt-2 text-left text-muted px-3">
-          <>address addressaddress address address address address </>
+          Mon - Sun <br/> 9:00 AM - 8:00 PM
         </div>
       </div>
-
-    )
-
-
+    );
 
     const copyRights = (
       <div className="col-12 col-md-3  mt-4">
         <div className="row mt-3 ml-1 justify-content-center">
-          <img src="assets/images/logo.png"/>
+          <img src="assets/images/logo.png" />
         </div>
         <div className="row justify-contnet-center mt-3">
           <div className="col-12 ml-2 text">
@@ -66,7 +64,6 @@ class ContactsComponent extends Component {
               All Rights Reserved{" "}
             </pre>
           </div>
-
         </div>
       </div>
     );
